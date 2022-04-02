b93db19236e1040979240300e8605d106e347753
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-04-02 04:41:27 -0500

    Video 1 commits. Adds delete button, user authentication, user-specific wishlists, details about a specific place, image uploads, updated style.css

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name} visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'
+        notes_str = self.notes[100:] if self.notes else 'no notes'
+        return f'{self.name} visited? {self.visited} {self.visited} on {self.date_visited}. Notes: {notes_str}. Photo {photo_str}'
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,35 +1,61 @@
+from turtle import ht
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
+@login_required
 def place_list(request):
 
     if request.method == 'POST':
         # create new place
         form = NewPlaceForm(request.POST) # creating a form from data in the request
-        place = form.save() # creating a model object from form
+        place = form.save(commit=False) # creating a model object from form
+        place.user = request.user
         if form.is_valid(): # validation against DB constraints
             place.save() # saves place to database
             return redirect('place_list') # reloads front page
 
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm() # used to create HTML
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+@login_required
 def about(request):
     author = 'Wyatt'
     about = 'A website to create a list of places to visit' # creates basic information about the author and website
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
 
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True) # filters information to only include places that were visited
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
+@login_required
 def place_was_visited(request, place_pk): # this method saves places that are marked as visited
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
-        place.visited = True
-        place.save()
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
     return redirect('place_list')
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    return render(request, 'travel_wishlist/place_detail.html', {'place': place})
+
+@login_required
+def delete_place(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect('place_list')
+
+    else:
+        return HttpResponseForbidden()
